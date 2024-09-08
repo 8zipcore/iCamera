baab9cbf0fc8a57a76d40c45d40b5145c6a288be
8zipcore <howtofindjayu@gmail.com>
2024-09-08 18:53:30 +0900

    Initial Commit

---
+++ iCamera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iCamera
+//
+//  Created by 홍승아 on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iCamera/iCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCameraApp.swift
+//  iCamera
+//
+//  Created by 홍승아 on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct iCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
