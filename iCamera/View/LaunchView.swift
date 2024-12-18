21d32d6e6225b9e3861a9f3534bc7c35e135c032
8zipcore <howtofindjayu@gmail.com>
2024-12-18 14:28:00 +0900

    LaunchView 수정

--- iCamera/View/LaunchView.swift
+++ iCamera/View/LaunchView.swift
@@ -16,29 +16,23 @@ struct LaunchView: View {
             ZStack{
                 LinearGradient(
                     gradient: Gradient(stops: [
-                        .init(color: .white, location: 0.4),
+                        .init(color: .white, location: 0.2),
                         .init(color: Colors.silver, location: 1.0)
                     ]),
                     startPoint: .top, // 시작점
                     endPoint: .bottom // 끝점
                 )
-                VStack(spacing: 0){
-                    Spacer()
-                    HStack(spacing: 0){
-                        Spacer()
-                        Image("blue_circle")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 16, height: 16)
-                            .padding(.bottom, -10)
-                            .padding(.trailing, -2)
-                    }
-                    Text("iCamera")
-                        .font(.system(size: 45, weight: .medium))
-                    Spacer()
-                }
-                .frame(width: viewWidth * 0.5)
-                .position(x: viewWidth / 2, y: viewHeight / 3)
+                let imageWidth = viewWidth * 0.4
+                
+                Image("logo")
+                    .resizable()
+                    .frame(width: imageWidth, height: imageWidth)
+                    .position(x: viewWidth / 2, y: viewHeight / 2)
+                
+                Text("iCamera")
+                    .font(.system(size: 18, weight: .medium))
+                    .foregroundStyle(.black)
+                    .position(x: viewWidth / 2, y: viewHeight)
             }
             .ignoresSafeArea()
         }
