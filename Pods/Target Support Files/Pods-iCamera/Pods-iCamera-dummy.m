4e29c67d99f34dd5c5a22b2c3ab13100eec961cf
8zipcore <howtofindjayu@gmail.com>
2025-09-20 21:45:29 +0900

    chore: CocoaPods 제거 후 SPM으로 전환

--- Pods/Target Support Files/Pods-iCamera/Pods-iCamera-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_iCamera : NSObject
-@end
-@implementation PodsDummy_Pods_iCamera
-@end
--- iCamera/Features/Edit/Cut/Manager/PixCropManager.swift
+++ iCamera/Features/Edit/Cut/Manager/PixCropManager.swift
@@ -5,7 +5,7 @@
 //  Created by 홍승아 on 2/9/25.
 //
 
-import Foundation
+import UIKit
 import PixCrop
 
 class PixCropManager: ObservableObject, Equatable {
